d03667a8c9634f80194ca8ea69b5c6a20eba4345
RM <rpmulrennan@gmail.com>
2021-11-07 14:42:12 -0600

    added modal open and close

--- src/Modal.js
+++ src/Modal.js
@@ -5,12 +5,50 @@ function Modal(props) {
     if(props.visibility===true){
     return (
         <div className="modal">
-            Hey there dudes
-            <br></br> 
-            Some more info for you
+            <div className="modal__content">
+                <div className="modal__head">
+                    Add a product
+                </div> 
+                <div className="modal__body"> 
+                    <form>
+                        <label>
+                        Product ID:
+                        <input type="int" />
+                        </label> <br></br> 
+                        <label>
+                        Description:
+                        <input type="text" />
+                        </label> <br></br> 
+                        <label>
+                        Quantity:
+                        <input type="int" />
+                        </label> <br></br> 
+                        <label>
+                        Unit:
+                        <input type="text" />
+                        </label> <br></br> 
+                        <label>
+                        Unit Price:
+                        <input type="int" />
+                        </label> <br></br> 
+                        <label>
+                        Subtotal:
+                        <input type="int" value={100} />
+                        </label> <br></br> 
+                        
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div> 
+                <div className="modal__foot">
+                    <button onClick={()=> props.close()}> Close </button>
 
-            <button onClick={()=> props.close()}> Close </button>
-        </div>
+                </div> 
+            </div> 
+
+
+        </div> 
+
+    
     )
     } 
     else{
